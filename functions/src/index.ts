feac39f7fce0c2765b603def6fc2f36ec2fcd0b3
陶 金 <kinto@dcs.co.jp>
2018-07-19 15:46:11 +0900

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,40 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { TextToSpeechClient } from '@google-cloud/text-to-speech';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// init
+admin.initializeApp(functions.config().firebase);
+const bucket = admin.storage().bucket();
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  // unsupport
+  if (request.method === 'GET') {
+    response.status(404).end();
+    return;
+  }
+
+  const { text, languageCode = 'ja-JP', ssmlGender = 'FEMALE', audioEncoding = 'MP3' } = request.body;
+
+  const client: TextToSpeechClient = new TextToSpeechClient();
+  const speechReq = {
+    input: { text },
+    voice: { languageCode, ssmlGender },
+    audioConfig: { audioEncoding },
+  };
+
+  client.synthesizeSpeech(speechReq, async (err, res: any) => {
+    if (err) {
+      response.status(400).send(err).end();
+      return;
+    }
+
+    const file = bucket.file(`${new Date().getTime()}.mp3`);
+    // save file
+    await file.save(res.audioContent);
+    const signedURL = await file.getSignedUrl({ action: 'read' });
+
+    response.status(200).send({
+      signedURL
+    });
+  });
+});
